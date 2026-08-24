7bd131a148565ab7ad20b06fb3d666773ca4974b
Kelly L. Boles <boleskellyl@gmail.com>
2026-08-24 03:04:53 -0400

    Changes to Week2 Slides

---
+++ week-2/Markdown_Styling.R
@@ -0,0 +1,22 @@
+# Load Data
+
+## Data Download Options
+- From Canvas
+- From Google Link
+
+## Steps:
+1. Go to Canvas
+2. Go to course module page
+3. Find the week
+4. Download zip file
+
+Store the zip file and extract it **in your course folder**.
+*You can delete the .zip file after you unzip it.*
+
+For the week-2 homework, you will be working with `gapminder_data`.
+
+
+
+
+
+
